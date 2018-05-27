47cd730fcd2dea11351ba143f782257662508e89
Anton Pelykh <anton.pelykh.dev@gmail.com>
2018-05-27 22:56:05 +0300

    Created more verification integration tests.

--- tests/functional/VerificationCest.php
+++ tests/functional/VerificationCest.php
@@ -42,4 +42,56 @@ class VerificationCest
             'status' => 'ENABLED',
         ]);
     }
+
+    public function testVerificationWithIncorrectId(FunctionalTester $I): void
+    {
+        /** @var \App\Entity\EmailVerificationRequest $verificationRequest */
+        $verificationRequest = $this->fixture->getReference(
+            EmailVerificationRequestFixtureLoader::REF_PENDING_REQUEST,
+            4
+        );
+
+        $I->haveContentTypeJson();
+        $I->sendPOST('/security/verification', [
+            'id' => $verificationRequest->getId() + 100,
+            'token' => $verificationRequest->getToken(),
+        ]);
+
+        $I->seeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'validationErrors' => [
+                [
+                    'property' => 'token',
+                    'message' => 'Request token is not valid.',
+                ],
+            ],
+        ]);
+    }
+
+    public function testVerificationWithIncorrectToken(FunctionalTester $I): void
+    {
+        /** @var \App\Entity\EmailVerificationRequest $verificationRequest */
+        $verificationRequest = $this->fixture->getReference(
+            EmailVerificationRequestFixtureLoader::REF_PENDING_REQUEST,
+            4
+        );
+
+        $I->haveContentTypeJson();
+        $I->sendPOST('/security/verification', [
+            'id' => $verificationRequest->getId(),
+            'token' => 'incorrect-token',
+        ]);
+
+        $I->seeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'validationErrors' => [
+                [
+                    'property' => 'token',
+                    'message' => 'Request token is not valid.',
+                ],
+            ],
+        ]);
+    }
 }
